5159abf2904e776e534a8027262e6db372c913c3
Agamemnon Theodorakopoulos <memos.t96@gmail.com>
2020-04-05 15:24:10 +0300

    test

--- app.js
+++ app.js
@@ -187,24 +187,24 @@ app.get("/profile", (req, res) => {
         Profile.setThread();
         res.write(`<p>Set Messenger Profile of Page ${config.pageId}</p>`);
       }
-      if (mode == "personas" || mode == "all") {
-        Profile.setPersonas();
-        res.write(`<p>Set Personas for ${config.appId}</p>`);
-        res.write(
-          "<p>To persist the personas, add the following variables \
-          to your environment variables:</p>"
-        );
-        res.write("<ul>");
-        res.write(`<li>PERSONA_BILLING = ${config.personaBilling.id}</li>`);
-        res.write(`<li>PERSONA_CARE = ${config.personaCare.id}</li>`);
-        res.write(`<li>PERSONA_ORDER = ${config.personaOrder.id}</li>`);
-        res.write(`<li>PERSONA_SALES = ${config.personaSales.id}</li>`);
-        res.write("</ul>");
-      }
-      if (mode == "nlp" || mode == "all") {
-        GraphAPi.callNLPConfigsAPI();
-        res.write(`<p>Enable Built-in NLP for Page ${config.pageId}</p>`);
-      }
+      // if (mode == "personas" || mode == "all") {
+      //   Profile.setPersonas();
+      //   res.write(`<p>Set Personas for ${config.appId}</p>`);
+      //   res.write(
+      //     "<p>To persist the personas, add the following variables \
+      //     to your environment variables:</p>"
+      //   );
+      //   res.write("<ul>");
+      //   res.write(`<li>PERSONA_BILLING = ${config.personaBilling.id}</li>`);
+      //   res.write(`<li>PERSONA_CARE = ${config.personaCare.id}</li>`);
+      //   res.write(`<li>PERSONA_ORDER = ${config.personaOrder.id}</li>`);
+      //   res.write(`<li>PERSONA_SALES = ${config.personaSales.id}</li>`);
+      //   res.write("</ul>");
+      // }
+      // if (mode == "nlp" || mode == "all") {
+      //   GraphAPi.callNLPConfigsAPI();
+      //   res.write(`<p>Enable Built-in NLP for Page ${config.pageId}</p>`);
+      // }
       if (mode == "domains" || mode == "all") {
         Profile.setWhitelistedDomains();
         res.write(`<p>Whitelisting domains: ${config.whitelistedDomains}</p>`);
