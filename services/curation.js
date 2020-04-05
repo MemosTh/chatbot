cf5070ec6c3cdbf248a436c117be845c94f7d803
Agamemnon Theodorakopoulos <memos.t96@gmail.com>
2020-04-05 16:09:03 +0300

    test

--- services/curation.js
+++ services/curation.js
@@ -12,70 +12,145 @@
 
 // Imports dependencies
 const Response = require("./response"),
-  config = require("./config"),
-  i18n = require("../i18n.config");
+    config = require("./config"),
+    i18n = require("../i18n.config");
 
 module.exports = class Curation {
-  constructor(user, webhookEvent) {
-    this.user = user;
-    this.webhookEvent = webhookEvent;
-  }
-
-  handlePayload(payload) {
-    let response;
-    let outfit;
-
-    switch (payload) {
-
-      case "CURATION":
-
-        let elements = [
-          {
-            image_url:'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
-            title:"Μαγείρεμα",
-            subtitle:"",
-            buttons:[]
-          },
-          {
-            image_url:'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
-            title:"Επίσημη αντιπροσωπεία Miele",
-            subtitle:"τεστ",
-            buttons:[]
-          }
-
-        ]
-
-
-        let buttons = [
-          {
-            type: "postback",
-            title: "view",
-            payload: "THIS_IS_TEST"
-          },
-          {
-          type: "postback",
-          title: "view",
-          payload: "THIS_IS_TEST2"
+    constructor(user, webhookEvent) {
+        this.user = user;
+        this.webhookEvent = webhookEvent;
     }
-        ]
-        let count= 0;
-        for (let element of elements){
-            element["buttons"].push(buttons[count])
-            count++;
+
+    handlePayload(payload) {
+        let response;
+        let outfit;
+
+        switch (payload) {
+
+            case "CURATION":
+
+                let elements = [
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Μαγείρεμα",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Συσκευές Ψύξης & Συντηρητές Κρασιών ",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Καφετιέρες",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Πλυντήρια πιάτων & ρούχων ",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Στεγνωτήρια",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Συσκευές Σιδερώματος",
+                        subtitle: "",
+                        buttons: []
+                    },
+                    {
+                        image_url: 'https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg',
+                        title: "Ηλεκτρικές σκούπες",
+                        subtitle: "",
+                        buttons: []
+                    },
+
+
+                ];
+
+
+                let buttons = [
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "COOK"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "FRIDGE"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "COFFEE"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "WASHER"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "DRY"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "IRON"
+                    },
+                    {
+                        type: "postback",
+                        title: "view",
+                        payload: "BROOM"
+                    },
+
+                ];
+                let count = 0;
+                for (let element of elements) {
+                    element["buttons"].push(buttons[count]);
+                    count++;
+                }
+
+                console.log(elements);
+
+                let list = Response.genGenericList(elements);
+                console.log(response);
+
+
+
+                let message = Response.genText("Ποια κατηγορία σας ενδιαφέρει;");
+                response= [message, list];
+                break;
+
+            case "COOK":
+                response = Response.genText("Ποια κατηγορία σας ενδιαφέρει;");
+                break;
+            case "FRIDGE":
+                break;
+            case "COFFEE":
+                break;
+            case "WASHER":
+                break;
+            case "DRY":
+                break;
+            case "IRON":
+                break;
+            case "BROOM":
+                break;
         }
 
-        console.log(elements);
-
-        response = Response.genGenericList(elements)
-        console.log(response);
-        break;
-
+        return response;
     }
-      let message = Response.genText("Ποια κατηγορία σας ενδιαφέρει;")
-    return [message, response];
-  }
-
-
 
 
 };
