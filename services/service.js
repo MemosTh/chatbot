98bb42693a1124f3073dff80cedeac30557b86f1
Agamemnon Theodorakopoulos <memos.t96@gmail.com>
2020-06-01 13:42:13 +0300

    test

--- services/service.js
+++ services/service.js
@@ -26,10 +26,41 @@ module.exports = class Service {
 
         switch (payload) {
             case "SERVICE":
-                let response = Response.genText("this is test ");
+                let elements = [
+                    {
+                        image_url:`${config.appUrl}/epikoinonia.jpg`,
+                        title: "Καλέστε μας στα τηλέφωνα 801 222 4444 & +30 210 67 94444",
+                        subtitle: "Δευτέρα έως Παρασκευή 09:00-17:00",
+                        buttons:[
+                            {
+                                type:"phone_number",
+                                title:"Κληση",
+                                payload: "+302106794444"
+                            }
+                        ]
+                    },
+                    {
+                        image_url: `${config.appUrl}/videoklisi.jpg`,
+                        title: "Βιντεοκλήση : Δείτε live τα προϊόντα μας στο 6974850525 με Viber, Messenger, Whatsapp:",
+                        subtitle: "Δευτ. - Παρ. 09:00-17:00",
+                        buttons:[
+                            {
+                                type:"phone_number",
+                                title:"Κληση",
+                                payload: "6974850525"
+                            }
+                        ]
+                    },
 
+
+                ];
+
+                let curation = Response.genGenericList(elements);
+                response =[curation]
+
+            break;
             case "SERVICE_TEST":
-                
+
 
 
         }
