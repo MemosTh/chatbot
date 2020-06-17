4a6ff021053090598fe1f6643ccf14df672aefee
Agamemnon Theodorakopoulos <memos.t96@gmail.com>
2020-06-17 16:18:25 +0300

    test

--- services/order.js
+++ services/order.js
@@ -31,8 +31,22 @@ module.exports = class Order {
 
         ];
 
-         response = Response.genGenericMedia("video", "https://www.facebook.com/Miele.Hellas/videos/962256810875854/", "cc");
+        var element =[
 
+          {
+            title: "Επίσημη αντιπροσωπεία Miele",
+            subtitle:"Καλώς ήλθατε στο e shop μας",
+            image_url: "https://www.miele.gr/media/domestic_gr/media/assets_288_x/20000154793_Zuschnitt.jpg",
+            default_action: {
+              type: "web_url",
+              url : "https://www.google.com/search?sxsrf=ALeKk03TdaoFAoycJW0WucjdrW6agd6maw:1588849668641&q=miele+service+greece&npsic=0&rflfq=1&rlha=0&rllag=38026745,23773810,6440&tbm=lcl&ved=2ahUKEwiO5-agzqHpAhWJxYUKHfQeAVAQjGp6BAgLEDQ&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u16!2m2!16m1!1e1!1m4!1u16!2m2!16m1!1e2!2m1!1e16!2m1!1e3!3sIAE,lf:1,lf_ui:4&rldoc=1#rlfi=hd:;si:;mv:[[42.73397854082312,34.90618672128997],[32.993780256537484,12.494077346289968],null,[38.02546067080482,23.700132033789966],6]",
+              webview_height_ratio: "full",
+            },
+            buttons:button1
+          },
+
+        ]
+        response = Response.genGenericTemplate(element)
     }
     return response;
   }
